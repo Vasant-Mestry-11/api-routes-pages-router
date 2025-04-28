2070b9628c7123fb01edfd4831cca1aac62ac5a5
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-28 23:32:58 +0530

    :zap: :art: :computer: feedback server api

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,28 @@
-
+import fs, { readFileSync } from 'fs'
 
 export default function feedbackHandler(req, res) {
+
+  if (req.method === "POST") {
+    
+    const { email, feedback } = req.body;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback
+    }
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath)
+    const data = JSON.parse(fileData);
+    data.push(newFeedback)
+    fs.writeFileSync(filePath, JSON.stringify(data))
+    res.status(201).json({
+      message: 'Success!',
+      feedback
+    })
+
+  }
   res.status(200).send({
     message: "This works"
   })
