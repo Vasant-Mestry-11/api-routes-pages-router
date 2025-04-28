aadb344d85dc9e17cdb5588785574b9062e40813
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-28 21:33:28 +0530

    :zap: :art: basic form structure

---
+++ pages/api/feedback.js
@@ -0,0 +1,7 @@
+
+
+export default function feedbackHandler(req, res) {
+  res.status(200).send({
+    message: "This works"
+  })
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
+import { useRef } from "react";
+
 export default function Home() {
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <>
-      
+      <h1>The home page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <input type="text" id="feedback" />
+        </div>
+      </form>
     </>
   );
 }
